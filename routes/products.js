db96133ce07ddedad94a819241b73b6406442da4
FefarDhruv <88278025+FefarDhruv@users.noreply.github.com>
2023-10-13 20:50:00 +0530

    Add files via upload

---
+++ routes/products.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/product');
+
+// Create a new product
+router.post('/', async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    await product.save();
+    res.status(201).json(product);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Get a list of all products
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
